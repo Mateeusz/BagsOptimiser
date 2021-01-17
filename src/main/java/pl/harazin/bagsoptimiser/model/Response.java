1ba1bb1378d6a46faa13e3e5b2516572510f30aa
Mateusz <mateuszharazin@wp.pl>
2021-01-17 17:23:16 +0100

    Improve statistics
    Signed-off-by: Mateusz <mateuszharazin@wp.pl>

--- src/main/java/pl/harazin/bagsoptimiser/model/Response.java
+++ src/main/java/pl/harazin/bagsoptimiser/model/Response.java
@@ -1,20 +1,32 @@
 package pl.harazin.bagsoptimiser.model;
 
+import lombok.Data;
+
 import java.util.Arrays;
 import java.util.List;
 
+@Data
 public class Response {
-    public List<List<Product>> result;
-    public float avgTime;
-    public float[] times;
+    private List<List<Product>> result;
+    private float avgTime;
+    private float[] times;
+    private int[] bagUsage;
+    private int avgBagUsage;
 
-    public Response(List<List<Product>> result, float[] measurements) {
+
+    public Response(List<List<Product>> result, float[] measurements, int[] bagUsage) {
         this.result = result;
         this.times = measurements;
+        this.bagUsage = bagUsage;
         float sum = 0;
+        int bags = 0;
         for (float measurement : measurements) {
             sum += measurement;
         }
+        for (int bag : bagUsage) {
+            bags += bag;
+        }
         this.avgTime = sum / measurements.length;
+        this.avgBagUsage = bags / bagUsage.length;
     }
 }
