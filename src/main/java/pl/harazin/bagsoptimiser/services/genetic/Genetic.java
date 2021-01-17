925c015c44b2566eab4a4b9b2cd91a2d5948e26a
Jakub Chmiel <kuba.chmiel2@gmail.com>
2021-01-17 11:33:19 +0100

    cr

--- src/main/java/pl/harazin/bagsoptimiser/services/genetic/Genetic.java
+++ src/main/java/pl/harazin/bagsoptimiser/services/genetic/Genetic.java
@@ -9,8 +9,6 @@ import java.util.*;
 
 @Service
 public class Genetic implements Algorithm {
-
-
     public static int POPULATION = 100;
     public static int REPRODUCTIVE_POPULATION = 30;
     public static int REPRODUCTIONS = 1000;
@@ -22,19 +20,14 @@ public class Genetic implements Algorithm {
 
         List<List<List<Product>>> population = PopulationGenerator.generate(inputProducts);
 
-
         return runAlgorithm(population, inputProducts);
-
-
     }
 
     private List<List<Product>> runAlgorithm(List<List<List<Product>>> population, List<Product> inputProducts) {
         Random rand = new Random();
         sortPopulation(population);
         for (int i = 0; i < GENERATIONS; i++) {
-
-
-            List<List<List<Product>>> reproductivePopulation = new ArrayList<>(population.subList(0, REPRODUCTIVE_POPULATION - 1));
+            List<List<List<Product>>> reproductivePopulation = new ArrayList(population.subList(0, REPRODUCTIVE_POPULATION - 1));
             //reproduction
             for (int j = 0; j < REPRODUCTIONS; j++) {
                 val parent1 = reproductivePopulation.get(rand.nextInt(reproductivePopulation.size()));
@@ -55,26 +48,20 @@ public class Genetic implements Algorithm {
                     randomBag2.remove(product2);
                     randomBag1.remove(product1);
                 }
-
             }
 
             sortPopulation(population);
             population = checkPopulation(population);
             if (population.size() > POPULATION)
                 population = population.subList(0, POPULATION);
-
-
         }
-
 
         return population.get(0);
     }
 
     private List<List<List<Product>>> checkPopulation(List<List<List<Product>>> population) {
-        List<List<List<Product>>> newPopulation = new ArrayList<>(population);
+        List<List<List<Product>>> newPopulation = new ArrayList(population);
         for (List<List<Product>> ind : population) {
-
-
             for (List<Product> bag : ind) {
                 int weight = 0;
                 int capacity = 0;
@@ -88,7 +75,6 @@ public class Genetic implements Algorithm {
                     newPopulation.remove(ind);
                     break;
                 }
-
             }
         }
         return newPopulation;
@@ -102,7 +88,6 @@ public class Genetic implements Algorithm {
         int[] set1 = generateSet(inputProducts.size(), parent1);
         int[] set2 = generateSet(inputProducts.size(), parent2);
 
-
         for (int i = cutPoint; i < set1.length; i++) {
             int temp = set1[i];
             set1[i] = set2[i];
@@ -112,30 +97,26 @@ public class Genetic implements Algorithm {
         List<List<Product>> child1 = setToList(set1, inputProducts);
         List<List<Product>> child2 = setToList(set1, inputProducts);
 
-        List<List<List<Product>>> children = new ArrayList<>();
+        List<List<List<Product>>> children = new ArrayList();
         children.add(child1);
         children.add(child2);
 
         return children;
-
-
     }
 
     private List<List<Product>> setToList(int[] set, List<Product> inputProducts) {
-        Map<Integer, List<Product>> map = new HashMap<>();
+        Map<Integer, List<Product>> map = new HashMap();
 
         for (int i = 0; i < set.length - 1; i++) {
             if (!map.containsKey(set[i])) {
-                List<Product> list = new ArrayList<>();
+                List<Product> list = new ArrayList();
                 list.add(inputProducts.get(i + 1));
                 map.put(set[i], list);
             } else {
                 map.get(set[i]).add(inputProducts.get(i + 1));
             }
         }
-        return new ArrayList<>(map.values());
-
-
+        return new ArrayList(map.values());
     }
 
     private int[] generateSet(int productAmount, List<List<Product>> individual) {
@@ -159,7 +140,5 @@ public class Genetic implements Algorithm {
                 return Integer.compare(o1.size(), o2.size());
             }
         });
-
     }
-
 }
